f2b6cb84a1e32dc553b4e0ad7313e51d17bab667
Natanael <natanaelbandeira@gmail.com>
2022-07-22 12:50:51 -0600

    Add Roboto font, add styling to menu, update website name, add menu font color and weight

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,17 @@
 import './Menu.scss';
 
 const Menu = () => (
-    <div className="background">
-        <p>Test</p>
-    </div>
+    <header className="background">
+        <nav className='navbar'>
+            <a href='#' className='logo'>
+                <img src="" alt="BGG Collection Finder logo"></img>
+            </a>
+            <ul className='nav-links'>
+                <li className='link'>List</li>
+                <li className='link'>About</li>
+            </ul>
+        </nav>
+    </header>
 );
 
 export default Menu;
