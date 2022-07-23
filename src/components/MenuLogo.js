6d95ef95bbefbc9e6a412bab3bec65100abed2f4
Natanael <natanaelbandeira@gmail.com>
2022-07-22 21:49:28 -0600

    Add alignment adjusts for logo and links

--- src/components/MenuLogo.js
+++ src/components/MenuLogo.js
@@ -1,8 +1,8 @@
 import './MenuLogo.scss';
 
 const MenuLogo = () => (
-    <a href='/' className='logo'>
-        <img src={require('../images/logo.png')} alt="BGG Collection Finder"></img>
+    <a href='/'>
+        <img className='logo' src={require('../images/logo.png')} alt="BGG Collection Finder"></img>
     </a>
 );
 
