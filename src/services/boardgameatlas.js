ecffd4f12f79b833f0a19d716f8203c6550d5a08
Natanael <natanaelbandeira@gmail.com>
2022-07-25 10:28:13 -0600

    Transform api request function in boardgameatlas service from promises to async-await

--- src/services/boardgameatlas.js
+++ src/services/boardgameatlas.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 const URL = `https://api.boardgameatlas.com/api/search?list_id=mGkyiPhujf&client_id=${process.env.REACT_APP_CLIENT_ID}`;
 
-const getAll = () => {
-    return axios
-        .get(URL)
-        .then(response => response.data.games);
+const getAll = async () => {
+    const response = await axios.get(URL);
+    return response.data.games;
 };
 
 export default { getAll };
